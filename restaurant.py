15f07a0ba17d5b811234bc478931e770ae8e2a54
arnavroh45 <58484869+arnavroh45@users.noreply.github.com>
2023-12-02 15:34:35 +0530

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-     
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+shortreview = []
+for i in range(0, 1000):
+  review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+  review = review.lower()
+  review = review.split()
+  ps = PorterStemmer()
+  all_stopwords = stopwords.words('english')
+  all_stopwords.remove('not')
+  review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+  review = ' '.join(review)
+  shortreview.append(review)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf' , random_state = 0)
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+accuracy_score(y_test, y_pred)
